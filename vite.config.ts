5e2d43a0421c09cd1a1c23036b33f802cb2da833
zhake2025 <zhake2025@users.noreply.github.com>
2025-10-02 08:32:36 +0800

    Fix base path from /w2 to /w1

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/w2',
+  base: '/w1',
   plugins: [
     react(),
     VitePWA({
